4fbc1816b2da5958dbf4f3f526b5bd24e7650e53
amatshiye <amatshiy@student.42.fr>
2018-07-02 08:56:15 +0200

    Removed debug messages

--- src/ErrorEngine.cpp
+++ src/ErrorEngine.cpp
@@ -23,7 +23,6 @@ ErrorEngine::ErrorEngine(void)
 
 ErrorEngine::ErrorEngine(std::string line, int words)
 {
-    std::cout << "Error Engine called" << std::endl;
     this->_line = line;
     this->_exit = false;
     this->_words = words;
@@ -39,7 +38,6 @@ ErrorEngine::ErrorEngine(const ErrorEngine &src)
 
 ErrorEngine::~ErrorEngine(void)
 {
-    std::cout << "ErrorEngine destructor called" << std::endl;
 }
 
 void    ErrorEngine::exception_core() throw()
@@ -75,26 +73,10 @@ std::string ErrorEngine::getErrorMessage()
     return this->_error_message;
 }
 
-//Exception details class
-/*
-ErrorEngine::ErrorDetails::ErrorDetails()
-{
-    
-    this->_errorMsg = "";
-}*/
-
 //Function to split the instruction per line
 std::vector<std::string> ErrorEngine::ISplit(std::string line, int words)
 {
-    //get the line
-    //get the number of words in a line
-    //words should be 1 or 2 per line
-    //if words = 2 use strsplit
-    //if 1 put it in the vector
     //vector size
-    std::cout << "==================================" << std::endl;
-    std::cout << "Line Value: " << line << std::endl;
-    std::cout << "==================================" << std::endl;
     std::vector<std::string> tokens;
     std::vector<std::string> instruction;
 
@@ -105,40 +87,30 @@ std::vector<std::string> ErrorEngine::ISplit(std::string line, int words)
 
     if (words == 1 && line.length() != 0)
     {
-        std::cout << "===========One word found==============" << std::endl;
-        std::cout << "Line Word 1 Test: " << line << std::endl;
         opCode = f_engine.removeSpace(line);
         instruction.push_back(opCode);
         std::cout << "opCode: " << opCode << std::endl;
-        std::cout << "================END====================" << std::endl;
     }
     else if (words == 2)
     {
-
         //getting opcode
-        std::cout << "================1st Code====================" << std::endl;
         tokens = f_engine.ft_strplit(line, " ");
         std::string temp = tokens.at(1);
         opCode = f_engine.removeSpace(tokens.at(0));
         instruction.push_back(opCode);
-        std::cout << "Opcode: " << opCode << std::endl;
+        std::cout << "Token Opcode: " << opCode << std::endl;
 
-        std::cout << "================2nd Code====================" << std::endl;
         //getting data type
         tokens = f_engine.ft_strplit(tokens.at(1), "(");
         dataType = f_engine.removeSpace(tokens.at(0));
         instruction.push_back(dataType);
-        std::cout << "Token dataType: " << dataType << std::endl;
+        std::cout << "Token datatype: " << dataType << std::endl;
 
-        //use find to get the position of the bracket
-        //use substr
-
-        std::cout << "================3rd Code====================" << std::endl;
         //getting value
         temp = temp.substr(dataType.length() + 1, temp.length());
         tokens = f_engine.ft_strplit(temp, ")");
         value = tokens.at(0);
-        std::cout << "Value: " << value << std::endl;
+        std::cout << "Token value: " << value << std::endl;
     }
     return instruction;
 }
