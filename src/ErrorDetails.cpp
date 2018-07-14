0aa104177caa19a2aecac70daf8d430830d7c5e2
Afrika MATSHIYE <amatshiy@e4r5p11.wethinkcode.co.za>
2018-07-14 10:28:52 +0200

    Project Finished

---
+++ src/ErrorDetails.cpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ErrorDetails.cpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amatshiy <amatshiy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/04 10:17:25 by amatshiy          #+#    #+#             */
+/*   Updated: 2018/07/04 10:36:30 by amatshiy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/ErrorDetails.hpp"
+
+ErrorDetails::ErrorDetails()
+{
+    this->_errorMsg = "";
+}
+
+ErrorDetails::ErrorDetails(std::string errorMsg)
+{
+    this->_errorMsg = errorMsg;
+}
+
+ErrorDetails::ErrorDetails(ErrorDetails const &src)
+{
+    *this = src;
+}
+
+ErrorDetails::~ErrorDetails(void) throw() {}
+
+ErrorDetails & ErrorDetails::operator=(const ErrorDetails &rhs)
+{
+    this->_errorMsg = rhs._errorMsg;
+    return *this;
+}
+
+std::string ErrorDetails::getErrorMsg() const
+{
+    return this->_errorMsg;
+}
+
+void    ErrorDetails::setErrorMsg(std::string errorMsg)
+{
+    this->_errorMsg = errorMsg;
+}
+
+const char  *ErrorDetails::what() const throw()
+{
+    const char * msg = this->getErrorMsg().c_str();
+    return msg;
+}
---
+++ src/ErrorEngine.cpp
@@ -0,0 +1,452 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ErrorEngine.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amatshiy <amatshiy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/26 13:52:10 by amatshiy          #+#    #+#             */
+/*   Updated: 2018/07/14 07:31:30 by amatshiy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/ErrorEngine.hpp"
+#include "../includes/FileEngine.hpp"
+
+ErrorEngine::ErrorEngine(void)
+{
+    this->_error_message = "";
+    this->_line = "";
+    this->_exit = false;
+    this->_words = 0;
+}
+
+ErrorEngine::ErrorEngine(std::string line, int words, int l_num)
+{
+    this->_line = line;
+    this->_exit = false;
+    this->_words = words;
+    this->_l_num = l_num - 1;
+
+    this->ISplit(this->_line, this->_words);
+    this->exception_core();
+}
+
+ErrorEngine::ErrorEngine(const ErrorEngine &src)
+{
+    *this = src;
+}
+
+ErrorEngine::~ErrorEngine(void)
+{
+}
+
+void    ErrorEngine::exception_core() throw()
+{
+    //call other error handling functions
+    if (this->_exit)
+        std::cout << this->getErrorMessage() << std::endl;
+}
+
+void   ErrorEngine::check_exit()
+{
+    if (this->_line == "exit")
+    {
+        this->_exit = true;
+    }
+}
+
+//deal with the fucken spaces man
+bool    ErrorEngine::check_stack()
+{
+    //check if stack is not greater than 2;
+    return false;
+}
+
+bool    ErrorEngine::is_stack_empty()
+{
+    //check if stack is empty
+    return false;
+}
+
+std::string ErrorEngine::getErrorMessage()
+{
+    return this->_error_message;
+}
+
+//Function to split the instruction per line
+void    ErrorEngine::ISplit(std::string line, int words)
+{
+    //vector size
+    std::vector<std::string> tokens;
+    std::vector<std::string> instruction;
+
+    FileEngine f_engine;
+    ErrorDetails e;
+    std::string opCode;
+    std::string dataType;
+    std::string value;
+
+    if (words == 1)
+    {
+        opCode = f_engine.removeSpace(line);
+        instruction.push_back(opCode);
+    }
+    else if (words == 2)
+    {
+        //getting opcode
+        tokens = f_engine.ft_strplit(line, " ");
+        std::string temp = tokens.at(1);
+        opCode = f_engine.removeSpace(tokens.at(0));
+        instruction.push_back(opCode);
+
+        //getting data type
+        //reason why the vector is funcking up ;)
+        tokens = f_engine.ft_strplit(tokens.at(1), "(");
+        dataType = f_engine.removeSpace(tokens.at(0));
+        instruction.push_back(dataType);
+
+        //getting value
+        temp = temp.substr(dataType.length() + 1, temp.length());
+        tokens = f_engine.ft_strplit(temp, ")");
+        value = tokens.at(0);
+        instruction.push_back(value);
+    }
+    else if (words > 2)
+    {
+        // throw an exception if shit went down
+        e.setErrorMsg("\033[1;31mError\033[0m: Multiple instructions in one line");
+        throw e;
+    }
+
+    if (instruction.size() > 0)
+    {
+        this->parseInstruction(instruction);
+    }
+    //return instruction;
+}
+
+void    ErrorEngine::parseInstruction(std::vector<std::string> instruction)
+{
+    // //check if opcode is valid
+    ErrorDetails e;
+
+    if (instruction.size())
+    {
+        FileEngine f_engine;
+        std::string opCodes[11] = {"pop", "dump", "add", "sub", "mul", "div", "mod", 
+                                    "print", "exit", "push", "assert"};
+
+        std::string dataTypes[5] = {"int8", "int16", "int32", "float", "double"};
+
+        if (!f_engine.in_array(instruction.at(0), opCodes, 11))
+        {
+            e.setErrorMsg("\033[1;31mError\033[0m: Invalid Opcode: " + instruction.at(0));
+            throw e;
+        }
+        else if (instruction.size() == 3)
+        {
+            if (!f_engine.in_array(instruction.at(1), dataTypes, 10))
+            {
+                e.setErrorMsg("\033[1;31mError\033[0m: Invalid data type: (" + instruction.at(1) + ") ERROR NO: " + std::to_string(this->_l_num));
+                throw e;
+            }
+            else
+            {
+                std::string opCode = instruction.at(0);
+                std::string dataType = instruction.at(1);
+                std::string value = instruction.at(2);
+
+                if (value.length())
+                {
+                    if (isConvertable(value, dataType))
+                    {
+                        if (dataType == "int8")
+                        {
+                            convertToINT8(value);
+                        }
+                        else if (dataType == "int16")
+                        {
+                            convertToINT16(value);
+                        }
+                        else if (dataType == "int32")
+                        {
+                            convertToINT32(value);
+                        }
+                        else if (dataType == "float")
+                        {
+                            convertToFLOAT(value);
+                        }
+                        else if (dataType == "double")
+                        {
+                            convertToDOUBLE(value);
+                        }
+                        else
+                        {
+                            ErrorDetails e_details("Error: Unknown Data type (" + dataType + ")");
+                            throw e_details;
+                        }
+                    }
+                    else
+                    {
+                        //std::cout << "We found the bug" << std::endl;
+                        ErrorDetails e_details("Error: Unable to convert value.");
+                        throw e_details;
+                    }
+                    //store var will be called here
+                    if (instruction.size() == 3)
+                    {
+                        this->_store.push_back(opCode);
+                        this->_store.push_back(dataType);
+                        this->_store.push_back(value);
+                    }
+                }
+                else
+                {
+                    ErrorDetails e_details("Error: No value found");
+                    throw e_details;
+                }
+            }
+        }
+        //add the opcode here
+        if (instruction.size() == 1)
+        {
+            this->_store.push_back(instruction.at(0));
+        }
+    }
+}
+
+std::vector<std::string>    ErrorEngine::getIns()
+{
+    return this->_store;
+}
+
+//checking if value is convertable 
+
+bool    ErrorEngine::isConvertable(std::string value, std::string &dataType)
+{
+    if ((dataType == "int8") || (dataType == "int16") 
+        || (dataType == "int32"))
+    {
+        try
+        {
+            int x = std::stod(value);
+            if (x == 0)
+                x++;
+            if (x)
+                return true;
+        }
+        catch(std::exception e)
+        {
+            return false;
+        }
+    }
+    else if (dataType == "float")
+    {
+        try
+        {
+            int x = std::stof(value);
+            if (x == 0)
+                x++;
+            if (x)
+                return true;
+        }
+        catch(std::exception e)
+        {
+            return false;
+        }
+    }
+    else if (dataType == "double")
+    {
+        try
+        {
+            int x = std::stod(value);
+            if (x == 0)
+                x++;
+            if (x)
+                return true;
+        }
+        catch(std::exception e)
+        {
+            return false;
+        }
+    }
+    return false;
+}
+
+//converting to int8
+
+void    ErrorEngine::convertToINT8(std::string value)
+{
+    int c_value;
+
+    try
+    {
+        c_value = std::stoi(value);
+    }
+    catch(std::exception e)
+    {
+        e.what();
+        ErrorDetails e_;
+        if (value[0] == '-')
+            e_.setErrorMsg("Error: (" + value + ") is less than INT8_MIN");
+        else
+            e_.setErrorMsg("Error: (" + value + ") is greather than INT8_MAX");
+        throw e_;
+    }
+
+
+    ErrorDetails e;
+
+    if (c_value > std::numeric_limits<int8_t>::max())
+    {
+        e.setErrorMsg("Error: (" + value + ") is greather than INT8_MAX");
+        throw e;
+    }
+    else if (c_value < std::numeric_limits<int8_t>::min())
+    {
+        e.setErrorMsg("Error: (" + value + ") is x less than INT8_MIN");
+        throw e;
+    }
+}
+
+//converting to int16
+
+void    ErrorEngine::convertToINT16(std::string value)
+{
+    int c_value;
+
+    try
+    {
+        c_value = std::stoi(value);
+    }
+    catch(std::exception e)
+    {
+        e.what();
+        ErrorDetails e_;
+        if (value[0] == '-')
+            e_.setErrorMsg("Error: (" + value + ") is less than INT16_MIN");
+        else
+            e_.setErrorMsg("Error: (" + value + ") is greather than INT16_MAX");
+        throw e_;
+    }
+
+    ErrorDetails e;
+
+    if (c_value > std::numeric_limits<int16_t>::max())
+    {
+        e.setErrorMsg("Error: (" + value + ") is greather than INT16_MAX");
+        throw e;
+    }
+    else if (c_value < std::numeric_limits<int16_t>::min())
+    {
+        e.setErrorMsg("Error: (" + value + ") is less than INT16_MIN");
+        throw e;
+    }
+}
+
+//converting to int32
+
+void    ErrorEngine::convertToINT32(std::string value)
+{
+    int32_t c_value;
+
+    try
+    {
+        c_value = std::stoi(value);
+    }
+    catch(std::exception e)
+    {
+        e.what();
+        ErrorDetails e_;
+        if (value[0] == '-')
+            e_.setErrorMsg("Error: (" + value + ") is less than INT32_MIN");
+        else
+            e_.setErrorMsg("Error: (" + value + ") is greather than INT32_MAX");
+        throw e_;
+    }
+
+
+    ErrorDetails e;
+
+    if (c_value > std::numeric_limits<int32_t>::max())
+    {
+        e.setErrorMsg("Error: (" + value + ") is greather than INT32_MAX");
+        throw e;
+    }
+    else if (c_value < std::numeric_limits<int32_t>::min())
+    {
+        e.setErrorMsg("Error: (" + value + ") is less than INT32_MIN");
+        throw e;
+    }
+}
+
+//converting to float
+
+void    ErrorEngine::convertToFLOAT(std::string value)
+{
+    float r_value;
+
+    try
+    {
+        r_value = std::stof(value);
+    }
+    catch(std::exception e)
+    {
+        e.what();
+        ErrorDetails e_;
+        if (value[0] == '-')
+            e_.setErrorMsg("Error: (" + value + ") is less than FLOAT_MIN");
+        else
+            e_.setErrorMsg("Error: (" + value + ") is greather than FLOAT_MAX");
+        throw e_;
+    }
+
+    ErrorDetails e;
+
+    if (r_value > std::numeric_limits<float>::max())
+    {
+        e.setErrorMsg("Error: (" + value + ") is greather than FLOAT_MAX");
+        throw e;
+    }
+    else if (r_value < std::numeric_limits<float>::min())
+    {
+        e.setErrorMsg("Error: (" + value + ") is less than FLOAT_MIN");
+        throw e;
+    }
+}
+
+//converting to double
+
+void    ErrorEngine::convertToDOUBLE(std::string value)
+{
+    double  r_value;
+
+    try
+    {
+        r_value = std::stof(value);
+    }
+    catch(std::exception e)
+    {
+        e.what();
+        ErrorDetails e_;
+        if (value[0] == '-')
+            e_.setErrorMsg("Error: (" + value + ") is less than DOUBLE_MIN");
+        else
+            e_.setErrorMsg("Error: (" + value + ") is greather than DOUBLE_MAX");
+        throw e_;
+    }
+
+    ErrorDetails e;
+
+    if (r_value > std::numeric_limits<double>::max())
+    {
+        e.setErrorMsg("Error: (" + value + ") is greather than DOUBLE_MAX");
+        throw e;
+    }
+    else if (r_value < std::numeric_limits<double>::min())
+    {
+        e.setErrorMsg("Error: (" + value + ") is less than DOUBLE_MIN");
+        throw e;
+    }
+}
